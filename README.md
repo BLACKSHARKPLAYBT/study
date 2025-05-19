1bce99d52c89d89e8a2c3043262fa1243c71d43b
BLACKSHARKPLAYBT <blscksharkplaybt@gmail.com>
2025-05-19 23:31:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# BILIBILI 数据库
---
+++ bilibili_db.sql
@@ -0,0 +1,44 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : Bilibili 数据库
+ Source Server Type    : MySQL
+ Source Server Version : 80040 (8.0.40)
+ Source Host           : mysql.sqlpub.com:3306
+ Source Schema         : bilibili_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 80040 (8.0.40)
+ File Encoding         : 65001
+
+ Date: 19/05/2025 23:21:56
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `uid` int NOT NULL,
+  `c_name` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` char(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `password` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` char(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `c_time` date NOT NULL,
+  `level` int NOT NULL,
+  `member` char(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `live` int NOT NULL,
+  `followed` int NOT NULL,
+  `follower` int NOT NULL,
+  `collection` int NOT NULL,
+  PRIMARY KEY (`uid`, `live`, `followed`, `follower`, `collection`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
